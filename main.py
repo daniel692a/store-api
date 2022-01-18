89525920a980131c403ef2f07262cb71037ec541
Daniel Armas Ramírez <daniel62armas@gmail.com>
2022-01-17 19:25:52 -0600

    Finish CRUD

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routes.user import user
 
-app = FastAPI(title="Store API", description="Store API", version="0.1")
+app = FastAPI(title="Store API", description="Store API", version="0.0.1", openapi_tags=[{"name": "users", "description": "User operations"}])
 
 app.include_router(user)
--- routes/user.py
+++ routes/user.py
@@ -1,11 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from config.db import conn
 from models.user import users
+from schemas.user import User
+from starlette.status import HTTP_204_NO_CONTENT, HTTP_200_OK
+import hashlib
 
 user = APIRouter()
 
-@user.get("/users")
+@user.get("/users", response_model=list[User], tags=["users"])
 def get_users():
     return conn.execute(users.select()).fetchall()
 
-@user.post("/users")
+@user.post("/users/create/", response_model=int, tags=["users"])
+def create_user(user: User):
+    new_user = {"name": user.name, "email": user.email}
+    new_user["password"] = user.password
+    result = conn.execute(users.insert().values(new_user))
+    return result.lastrowid
+
+@user.get("/users/{id}", response_model=User, tags=["users"])
+def get_users(id: int):
+    return conn.execute(users.select().where(users.c.id == id)).first()
+
+
+@user.delete("/users/delete/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
+def delete_user(id: int):
+    result = conn.execute(users.delete().where(users.c.id == id))
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+@user.put("/users/update/{id}", response_model=int, tags=["users"])
+def update_user(id: int, user: User):
+    conn.execute(users.update().values(
+        name=user.name,
+        email=user.email,
+        password=user.password).where(users.c.id == id))
+    return Response(status_code=HTTP_200_OK)
